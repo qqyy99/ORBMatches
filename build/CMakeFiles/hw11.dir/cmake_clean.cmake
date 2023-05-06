4d6294639a97db46a890fff8800cdc559aec0cee
linzs.online <linzs.online@gmail.com>
2023-05-06 17:14:03 +0800

    first commit

---
+++ build/CMakeFiles/hw11.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw11.dir/src/apps/hw11.cpp.o"
+  "CMakeFiles/hw11.dir/src/apps/hw11.cpp.o.d"
+  "hw11"
+  "hw11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/include/ORB/global_defination/global_defination.h
@@ -0,0 +1,10 @@
+#ifndef ORB_GLOBAL_DEFINATION_H_IN_
+#define ORB_GLOBAL_DEFINATION_H_IN_
+
+#include <string>
+
+namespace ORB {
+    const std::string WORK_SPACE_PATH = "/home/auto/zqy/projects/hw-day11";
+}
+
+#endif
